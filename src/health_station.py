94bcb7e78255ef8f4619093dfdaa31f9199eac85
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-13 12:13:46 +0100

    Update health_station.py

--- src/health_station.py
+++ src/health_station.py
@@ -1,4 +1,9 @@
+from person import Person
+
 class HealthStation:
+    
+    def __init__(self):
+        # what should be here?
 
     def weigh(self, person):
         # return the weight of the person passed as the parameter
